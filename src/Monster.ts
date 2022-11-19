e82e96453cfd0b7252f5f801acac5b83dcd7909e
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-11-19 16:07:57 -0300

    Requirement 12: Done

--- src/Monster.ts
+++ src/Monster.ts
@@ -3,8 +3,8 @@ import calculateDamage from './utils/calculateDamage';
 import calculateLife from './utils/calculateLife';
 
 export default class Monster implements SimpleFighter {
-  private _lifePoints: number;
-  private _strength: number;
+  protected _lifePoints: number;
+  protected _strength: number;
 
   constructor() {
     this._lifePoints = 85;
