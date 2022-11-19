6156d03b6967dce5c928b42c028bdd43bd67a25d
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-11-19 15:03:59 -0300

    Requirement 09: Done

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import { SimpleFighter } from './Fighter';
+import calculateDamage from './utils/calculateDamage';
+import calculateLife from './utils/calculateLife';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damageReceived = calculateDamage(attackPoints);
+    this._lifePoints = calculateLife(damageReceived, this._lifePoints);
+    return this._lifePoints;
+  }
+}
